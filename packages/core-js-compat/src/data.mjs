44072dd489d5880b4cfb88c2046da7e57710016c
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-18 06:28:51 +0700

    remove `esnext.map.update-or-insert` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1531,9 +1531,6 @@ export const data = {
   },
   'esnext.map.update': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.map.update-or-insert': {
-  },
   'esnext.map.upsert': {
   },
   'esnext.math.clamp': {
