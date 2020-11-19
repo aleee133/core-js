171816c8410a087f698a6bd432724d4ab8f43663
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-19 12:53:39 +0700

    remove `esnext.object.iterate-{ entries, keys, values }` (withdrawn)

--- packages/core-js/features/object/index.js
+++ packages/core-js/features/object/index.js
@@ -1,7 +1,4 @@
 var parent = require('../../stable/object');
 require('../../modules/esnext.object.has-own');
-require('../../modules/esnext.object.iterate-entries');
-require('../../modules/esnext.object.iterate-keys');
-require('../../modules/esnext.object.iterate-values');
 
 module.exports = parent;
